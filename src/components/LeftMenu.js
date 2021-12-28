a3c95b76b1d8f8abe29cbbc058205009f2856227
techshomez <danieltopeg@gmail.com>
2021-12-28 03:40:50 +0100

    updated

--- src/components/LeftMenu.js
+++ src/components/LeftMenu.js
@@ -3,25 +3,27 @@ import '../Styles/LeftMenu.css';
 
 import { FaSpotify, FaEllipsisH } from "react-icons/fa";
 import {BiSearchAlt} from "react-icons/bi";
+import { Menu } from "./Menu";
 
 const LeftMenu = () => {
     return (
         <div className="leftMenu">
             <div className="logoContainer">
                 <i>
-                   <FaSpotify/>
+                   <FaSpotify />
                 </i>
                 <h2>Spotify</h2>
                 <i>
-                  <FaEllipsisH/>
+                  <FaEllipsisH />
                 </i>
             </div>
             <div className="searchBox">
                 <input type="text" placeholder="Search..." />
                 <i className="searchIcon">
-                    <BiSearchAlt/>
+                    <BiSearchAlt />
                 </i>
             </div>
+            <Menu />
         </div>
     )
 }

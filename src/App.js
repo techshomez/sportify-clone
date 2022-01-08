da817ef9b48a2d711b61b8b37d7abc8134a37547
techshomez <danieltopeg@gmail.com>
2022-01-08 21:54:08 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { Banners } from "./components/Banner";
+import Solutions, { Banners } from "./components/Banner";
 import Header from "./components/Header";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banners />
+      <Solutions />
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,13 +1,43 @@
 import React from "react";
+import {BsFillMegaphoneFill} from 'react-icons/bs';
 
 import '../styles/Banner.css';
+import banner_images from '../images/cta-1-moc-1.png';
 
 const Banners = () => {
     return (
         <section className="section_banner">
-            hi
+            <div className="sty">
+               <div className="content">
+                 <h4>Hire Talent</h4>
+                 <h3>Find your dream job or talented employee</h3>
+                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi sit alias ad? Quia, enim hic. Error enim amet consectetur velit itaque quisquam, id sit dolorem non voluptatibus molestias facere tempora!
+                 </p>
+               </div>
+
+               <div className="image">
+                 <img src={ banner_images } alt=""/>
+               </div>
+
+           </div>
+            
         </section>
     );
 }
 
-export {Banners}
+const Solutions = () => {
+    return (
+        <section className="section_solution">
+            <div className="solution_container">
+                <h2>One platform Many Solutions</h2>                
+                
+            </div>
+        </section>
+    )
+}
+
+export default Solutions
+
+
+
+export { Banners }
